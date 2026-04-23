f42526491eae9b0559eb74f4546c88ed006e7184
Abdullah <abdullahmohmmad14@gmail.com>
2026-04-23 08:36:12 +0300

    Complete Lab 8

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -5,6 +5,25 @@ urlpatterns = [
  path('list_books/', views.list_books, name= "books.list_books"),
  path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
  path('aboutus/', views.aboutus, name="books.aboutus"),
+ path('html5/links/', views.links, name="books.links"),
+path('html5/text/formatting/', views.formatting, name="books.formatting"),
+path('html5/listing/', views.listing, name="books.listing"),
+path('html5/tables/', views.tables, name="books.tables"),
+path('search/', views.search, name="books.search"),
 
+path('simple/query', views.simple_query, name="simple_query"),
 
+path('complex/query', views.complex_query, name="complex_query"),
+
+path('lab8/task1/', views.lab8_task1, name='books.lab8_task1'),
+
+path('lab8/task2/', views.lab8_task2, name='books.lab8_task2'),
+
+path('lab8/task3/', views.lab8_task3, name='books.lab8_task3'),
+
+path('lab8/task4/', views.lab8_task4, name='books.lab8_task4'),
+
+path('lab8/task5/', views.lab8_task5, name='books.lab8_task5'),
+
+path('lab8/task7/', views.lab8_task7, name='books.lab8_task7'),
 ]
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
+from django.db.models import Q
+from django.db.models import Count, Sum, Avg, Max, Min
+from .models import Book, Address, Student
+from django.db.models import Count, Sum, Avg, Max, Min
+
 
 def index(request):
  name = request.GET.get("name") or "world!" #add this line
@@ -27,3 +33,83 @@ def viewbook(request, bookId):
 def aboutus(request):
  return render(request, 'bookmodule/aboutus.html')
 
+def links(request):
+    return render(request, 'bookmodule/links.html')
+
+def formatting(request):
+    return render(request, 'bookmodule/formatting.html')
+
+def listing(request):
+    return render(request, 'bookmodule/listing.html')
+
+def tables(request):
+    return render(request, 'bookmodule/tables.html')
+
+def search(request):
+ if request.method == "POST":
+  string = request.POST.get('keyword').lower()
+  isTitle = request.POST.get('option1')
+  isAuthor = request.POST.get('option2')
+  # now filter
+  books = __getBooksList()
+  newBooks = []
+  for item in books:
+   contained = False
+   if isTitle and string in item['title'].lower(): contained = True
+   if not contained and isAuthor and string in item['author'].lower(): contained = True
+
+   if contained: newBooks.append(item)
+  return render(request, 'bookmodule/bookList.html', {'books': newBooks})
+ return render(request, 'bookmodule/search.html')
+
+def simple_query(request):
+	mybooks=Book.objects.filter(title__icontains='and') # <- multiple objects
+	return render(request, 'bookmodule/bookList.html', {'books':mybooks})
+
+def complex_query(request):
+	mybooks=books=Book.objects.filter(author__isnull = False).filter(title__icontains='and').filter(edition__gte = 2).exclude(price__lte = 100)[:10]
+	if len(mybooks)>=1:
+ 	 return render(request, 'bookmodule/bookList.html', {'books':mybooks})
+	else:
+	 return render(request, 'bookmodule/index.html')
+
+def __getBooksList():
+ book1 = {'id':12344321, 'title':'Continuous Delivery', 'author':'J.Humble and D. Farley'}
+ book2 = {'id':56788765,'title':'Reversing: Secrets of Reverse Engineering', 'author':'E. Eilam'}
+ book3 = {'id':43211234, 'title':'The Hundred-Page Machine Learning Book', 'author':'Andriy Burkov'}
+ return [book1, book2, book3]
+
+
+def lab8_task1(request):
+    books = Book.objects.filter(Q(price__lte=80))
+    return render(request, 'bookmodule/lab8_task1.html', {'books': books})
+
+
+def lab8_task2(request):
+ books = Book.objects.filter(Q(edition__gt=3) & (Q(title__icontains='qu') | Q(author__icontains='qu')))
+ return render(request, 'bookmodule/lab8_task2.html', {'books': books})
+
+
+def lab8_task3(request):
+ books = Book.objects.filter(~Q(edition__gt=3) & ~(Q(title__icontains='qu') | Q(author__icontains='qu')))
+ return render(request, 'bookmodule/lab8_task3.html', {'books': books})
+
+
+def lab8_task4(request):
+ books = Book.objects.all().order_by('title')
+ return render(request, 'bookmodule/lab8_task4.html', {'books': books})
+
+
+def lab8_task5(request):
+    data = Book.objects.aggregate(
+        total_books=Count('id'),
+        total_price=Sum('price'),
+        avg_price=Avg('price'),
+        max_price=Max('price'),
+        min_price=Min('price')
+    )
+    return render(request, 'bookmodule/lab8_task5.html', {'data': data})
+
+def lab8_task7(request):
+    cities = Address.objects.annotate(student_count=Count('student'))
+    return render(request, 'bookmodule/lab8_task7.html', {'cities': cities})
